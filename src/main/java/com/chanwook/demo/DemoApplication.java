5fd496481bfee0f7bb4326b1922747a96bb6e18f
togetherwelove <rkdcksdnr1@gmail.com>
2023-11-06 22:59:01 +0900

    메인 클래스 수정 	modified:   src/main/java/com/chanwook/demo/DemoApplication.java

--- src/main/java/com/chanwook/demo/DemoApplication.java
+++ src/main/java/com/chanwook/demo/DemoApplication.java
@@ -3,10 +3,7 @@ package com.chanwook.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lombok.RequiredArgsConstructor;
-
 @SpringBootApplication
-@RequiredArgsConstructor
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
